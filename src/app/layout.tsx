00b149b18c399acab9ca712e42a1b4e4fd803d34
Felipe Santos <dev.felipesantos@gmail.com>
2023-10-30 16:53:54 -0300

    feat: add shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html suppressHydrationWarning lang="pt-BR">
+    <html suppressHydrationWarning lang={'pt-BR'}>
       <body className={inter.className}>{children}</body>
     </html>
   )
