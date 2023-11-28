0322dd80b764415f04c868efead5dfd0ca2d917e
Felipe Santos <dev.felipesantos@gmail.com>
2023-11-28 10:04:09 -0300

    feat: add shadcn button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+import Link from 'next/link'
+
 import MaxWidthWrapper from './components/max-width-wrapper'
+import { buttonVariants } from '@/components/ui/button'
 
 export default function Home() {
   return (
@@ -7,6 +10,16 @@ export default function Home() {
         <h1 className={`text-4xl font-bold tracking-tighter text-gray-900 sm:text-6xl `}>
           O mais completo site sobre <span className={`text-red-600`}>Demon Slayer</span>.
         </h1>
+
+        <p className={`mt-6 text-lg max-w-prose text-muted-foreground`}>
+          Aproveite o melhor conteúdo sobre o anime e mangá de Kimetsu no Yaiba.
+        </p>
+
+        <div className={`flex flex-col sm:flex-row gap-4 mt-6`}>
+          <Link href={`/personagens`} className={buttonVariants()}>
+            Personagens
+          </Link>
+        </div>
       </div>
     </MaxWidthWrapper>
   )
