3ff8b020455286a19130d29d7649390a96b1d6d8
Felipe Santos <dev.felipesantos@gmail.com>
2023-11-29 08:15:17 -0300

    refactor: alter title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <MaxWidthWrapper>
       <div className={`py-20 mx-auto text-center flex flex-col items-center max-w-3xl`}>
         <h1 className={`text-4xl font-bold tracking-tighter text-gray-900 sm:text-6xl `}>
-          O mais completo site sobre <span className={`text-red-600`}>Demon Slayer</span>.
+          O portal mais completo sobre <span className={`text-red-600`}>Demon Slayer</span>.
         </h1>
 
         <p className={`mt-6 text-lg max-w-prose text-muted-foreground`}>
